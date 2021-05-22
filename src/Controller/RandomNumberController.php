ae14f31c5f3500796a80121d1a3c0401c2df9df2
Vladimir <vladimir.solomka@gmail.com>
2021-05-22 23:58:22 +0300

    Homework 14: fixes

--- src/Controller/RandomNumberController.php
+++ src/Controller/RandomNumberController.php
@@ -6,7 +6,6 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\Routing\Annotation\Route;
 
 class RandomNumberController extends AbstractController
 {
